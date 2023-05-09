9ff1e9978af911e6c4272df6bc9959e6782f52d8
P05h <121305788+P05h@users.noreply.github.com>
2023-05-09 16:51:10 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@ self.addEventListener('fetch', event => {
   // Check if the request is being made to your domain
   if (request.url.includes("https://P05h.github.io")) {
     // Modify the request to point to the original website's server
-    const originalUrl = request.url.replace("https://P05h.github.io", 'https://tiw.tiwweb.workers.dev');
+    const originalUrl = request.url.replace("https://P05h.github.io/bare", 'https://tiw.tiwweb.workers.dev');
     
     // Fetch the modified request from the original website's server
     event.respondWith(fetch(originalUrl));
